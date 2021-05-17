975157eda9d7ffcdec6e45a157fe897ad6fda09c
sumit <sumitsarkar338@gmail.com>
2021-05-17 23:43:05 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,5 @@
 from flask import Flask, json, request
-
+from db import *
 app = Flask(__name__)
 
 
@@ -13,7 +13,23 @@ def github_api():
     if request.headers['Content-Type'] == 'application/json':
         l = request.json
         print(l)
-        print(type(l))
+        if str(l).find('pull_request')==False and str(l).find('before'):
+            return l
+        elif str(l).find('before'):
+            print("merge")
+            return l
+        elif str(l).find('pull_request'):
+            pull_req_by=l
+            request_id = pull_req_by['pull_request']['id']
+            author = pull_req_by['pull_request']['user']['login']
+            action = 'PULL_REQUEST'
+            from_branch = pull_req_by['pull_request']['head']['label']
+            to_branch = pull_req_by['pull_request']['base']['label']
+            query = {'request_id': request_id, 'author': author, 'action': action, 'from_branch': from_branch,
+                     'to_branch': to_branch}
+            print(query)
+            pull_ob=Pull(request_id,author,action,from_branch,to_branch)
+            pull_ob.commit(pull_ob.__dict__)
 
         return l
 
