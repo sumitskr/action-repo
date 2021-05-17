4737617553868372c2e6d6d241c03132e2ec781d
sumit <sumitsarkar338@gmail.com>
2021-05-17 19:33:46 +0530

    up py

--- app.py
+++ app.py
@@ -1,19 +1,22 @@
-from flask import Flask , json , request
-
+from flask import Flask, json, request
 
 app = Flask(__name__)
+
 
 @app.route('/')
 def index():
     return "sucess"
-@app.route('/github',methods=['POST'])
+
+
+@app.route('/github', methods=['POST'])
 def github_api():
-    if request.headers['Content-Type'] =='application/json':
-        l=request.json
+    if request.headers['Content-Type'] == 'application/json':
+        l = request.json
         print(l)
         print(type(l))
 
         return l
-if __name__== '__main__':
-    
+
+
+if __name__ == '__main__':
     app.run(debug=True)
