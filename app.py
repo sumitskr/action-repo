19da41adbfc378474f40488e3658701362e08fee
sumit <sumitsarkar338@gmail.com>
2021-05-17 15:26:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -8,14 +8,13 @@ def index():
 @app.route('/github',methods=['POST'])
 def github_api():
     if request.headers['Content-Type'] =='application/json':
-        l=json.dumps(request.json)
-        replaced_data=l.replace('true','True')
-        replaced_data=replaced_data.replace('false','False')
-        replaced_data=replaced_data.replace('null','None')
-        print("working")
-        print(type(replaced_data))
-        print(replaced_data['after'])
-        return replaced_data
+        l=request.json
+        # replaced_data=l.replace('true','True')
+        # replaced_data=replaced_data.replace('false','False')
+        # replaced_data=replaced_data.replace('null','None')
+        # print("working")
+        # print(type(replaced_data))
+        return l
     else:
         return "working"
 if __name__== '__main__':
