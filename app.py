107ee4d02f3bbd4526713fa3c04cd7c7c3aa357e
sumit <sumitsarkar338@gmail.com>
2021-05-17 15:34:09 +0530

    Update app.py

--- app.py
+++ app.py
@@ -9,11 +9,6 @@ def index():
 def github_api():
     if request.headers['Content-Type'] =='application/json':
         l=request.json
-        # replaced_data=l.replace('true','True')
-        # replaced_data=replaced_data.replace('false','False')
-        # replaced_data=replaced_data.replace('null','None')
-        # print("working")
-        # print(type(replaced_data))
         print(l)
         print(type(l))
         return l
