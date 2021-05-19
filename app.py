49e5239fea038847a94db58830b24f4ee2b90966
sumit <sumitsarkar338@gmail.com>
2021-05-19 09:39:17 +0530

    j

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, json, request,render_template
+from flask import Flask, json, request
 from db import *
-
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
-    activity_list=activity.find()
-    return render_template('index.html',activity_list=activity_list)
+    return "sucess"
 
 
 @app.route('/github', methods=['POST'])
@@ -15,11 +13,14 @@ def github_api():
     if request.headers['Content-Type'] == 'application/json':
         l = request.json
         print(l)
-        if str(l).find('before') != -1 and str(l).find('pull_request') == -1:
+        if str(l).find('pull_request')==True and str(l).find('before')==True:
+            print("its merge")
+            return l
+        elif str(l).find('before')==True and str(l).find('pull_request')==False:
             print("push")
             return l
-        elif str(l).find('pull_request') != -1 and str(l).find('before') == -1:
-            pull_req_by = l
+        elif str(l).find('pull_request')==True and str(l).find('before')==False:
+            pull_req_by=l
             request_id = pull_req_by['pull_request']['id']
             author = pull_req_by['pull_request']['user']['login']
             action = 'PULL_REQUEST'
@@ -28,7 +29,7 @@ def github_api():
             query = {'request_id': request_id, 'author': author, 'action': action, 'from_branch': from_branch,
                      'to_branch': to_branch}
             print(query)
-            pull_ob = Pull(request_id, author, action, from_branch, to_branch)
+            pull_ob=Pull(request_id,author,action,from_branch,to_branch)
             pull_ob.commit()
 
             return l
